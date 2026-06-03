c1376f4e0d36aae844c10983508302cd336caa23
n0n4w3-jj <story.botsfordli@gmail.com>
2026-06-03 21:12:45 +0800

    chore: remove footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 
-import Footer from './components/Footer';
 import MusicPlayer from './components/MusicPlayer';
 import BlogPostList from './components/BlogPostList';
 
@@ -26,7 +25,6 @@ function BlogPage() {
           revealProgress={revealProgress}
         />
       </main>
-      <Footer revealProgress={revealProgress} />
     </>
   );
 }
